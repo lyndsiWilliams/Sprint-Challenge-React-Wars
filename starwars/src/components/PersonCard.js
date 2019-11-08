bdf840d7c91d7bbcb0478ce5bd8d48978bc71258
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-08 16:19:24 -0600

    Got project to MVP

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import React, {useState} from 'react';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 const PersonCard = (props) => {
-    console.log(props.person.name);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+  
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
   return (
     <div>
-      <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>{props.person.name}</CardTitle>
-          <CardSubtitle>Birth year: {props.person.birth_year}</CardSubtitle>
+      <Card body inverse style={{
+          backgroundColor: 'indigo',
+          display: 'flex',
+          alignItems: 'center'
+          }}>
+        {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
+        <CardBody body inverse style={{
+            backgroundColor: 'thistle',
+            width: '50%',
+            color: 'midnightblue'
+            }}>
+          <CardTitle>Name: {props.name}</CardTitle>
+          <CardSubtitle>Birth year: {props.year}</CardSubtitle>
           <CardText>Click the button below for more information:</CardText>
-          <Button>More information</Button>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret>
+                More information
+                </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem disabled>Eye color: {props.eyeColor}</DropdownItem>
+                <DropdownItem disabled>Gender: {props.gender}</DropdownItem>
+                <DropdownItem disabled>Hair Color: {props.hairColor}</DropdownItem>
+                <DropdownItem disabled>Height: {props.height}</DropdownItem>
+                <DropdownItem disabled>Mass: {props.mass}</DropdownItem>
+                <DropdownItem disabled>Skin Color: {props.skinColor}</DropdownItem>
+            </DropdownMenu>
+            </Dropdown>
         </CardBody>
       </Card>
     </div>
--- starwars/src/components/PersonList.js
+++ starwars/src/components/PersonList.js
@@ -4,22 +4,43 @@ import PersonCard from './PersonCard';
 
 export default function PersonList() {
     const [person, setPerson] = useState([]);
-    console.log(person.name);
+    console.log(person);
     
     useEffect(() => {
-        axios.get('https://swapi.co/api/people/1')
+        axios.get('https://swapi.co/api/people/')
         .then(response => {
-          setPerson(response.data);
-          console.log(response.data);
+            setPerson(response.data.results);
+            console.log(response.data.results[0].name);
         })
         .catch(error => {
           console.log("The data was not returned.", error)
         })
       }, [])
 
+    //   const personArray = person.results;
+    //   console.log(personArray);
+
       return (
           <div className="listCont">
-          <PersonCard person={person} />
+              {person.map(per => {
+                //   console.log(per.name);
+                  return (
+                    <PersonCard
+                        person={per}
+                        key={per.url}
+                        name={per.name}
+                        year={per.birth_year}
+                        eyeColor={per.eye_color}
+                        gender={per.gender}
+                        hairColor={per.hair_color}
+                        height={per.height}
+                        mass={per.mass}
+                        skinColor={per.skin_color}
+                    />
+                  );
+              })}
+              {/* <PersonCard person={person} /> */}
+              Test
           </div>
       )
 }
